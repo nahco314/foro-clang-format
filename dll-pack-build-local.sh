684eac20ade23938789ef96238a77d16bbf638aa
nahco314 <nahco3_ta@yahoo.co.jp>
2024-12-22 02:20:46 +0900

    fix: fix build on macos

--- dll-pack-build-local.sh
+++ dll-pack-build-local.sh
@@ -2,6 +2,9 @@ sh ./build.sh
 
 mkdir ./artifacts/
 
-dll-pack-builder local foro-clang-format \
+LD_LIBRARY_PATH=$(pwd)/build/build/_deps/llvm_project-src/llvm/lib/ \
+    dll-pack-builder local foro-clang-format \
     $(dll-pack-builder find ${BUILD_OUT_DIR}) \
-    ./artifacts/ ${DLL_PACK_TARGET} ${GITHUB_REPOSITORY} ${GITHUB_REF#refs/tags/}
+    ./artifacts/ ${DLL_PACK_TARGET} ${GITHUB_REPOSITORY} ${GITHUB_REF#refs/tags/} \
+    --include "$(pwd)/build/**" \
+    --macho-rpath $(pwd)/build/build/_deps/llvm_project-src/llvm/lib/
